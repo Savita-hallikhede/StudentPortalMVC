52a29044808fea5876833b4c7b525ba8c8ba381d
savita <savitahallikhede11@gmail.com>
2026-07-21 02:10:15 +0530

    Add project files.

---
+++ StudentPortalMVC/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentPortalMVC.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+//[Authorize]
+
+//Means:
+
+//👉 Only logged-in users can access this controller.
+
+//If the user is not logged in, ASP.NET Identity automatically redirects them to the Login page.
